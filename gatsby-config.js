7b169a09653d4bbb70371558c6b9a4de13001a0c
pypeng28 <104598489+pypeng28@users.noreply.github.com>
2022-11-12 16:08:38 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://pypeng28.github.io/running_page/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
+        name: 'Bilibili',
+        url: 'https://space.bilibili.com/1511525',
       },
       {
         name: 'About',
